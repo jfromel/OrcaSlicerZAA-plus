01d6074d7192ec4aa84e65d5d806f4929fffafab
John Fromel <johnfromel@MacBook-Pro.local>
2026-04-29 15:10:59 -0700

    Trigger CI build

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -11065,3 +11065,4 @@ bool is_XL_printer(const PrintConfig &cfg)
 #include <cereal/types/polymorphic.hpp>
 CEREAL_REGISTER_TYPE(Slic3r::DynamicPrintConfig)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(Slic3r::DynamicConfig, Slic3r::DynamicPrintConfig)
+// OrcaSlicerZAA-plus
